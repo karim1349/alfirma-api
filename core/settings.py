99558b64b067ee84f2dcd3b2fa5979de040e6c37
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-11-09 15:56:03 +0100

    cache ignored

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-*_kh3gdfkuctuv+sq-g^bmw==uty4tg&e=vs=tavg2hy^5!%32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
